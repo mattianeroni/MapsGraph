617f32fdee504375bf965f5f02a72933f2398376
mattianeroni <54416801+mattianeroni@users.noreply.github.com>
2020-11-17 22:32:42 +0100

    Update graph.py

--- graph/graph.py
+++ graph/graph.py
@@ -1,28 +1,34 @@
-from __future__ import annotations
 """
 In this file everything concerning the creation of a graph is contained.
 
 
+< NODES >
+
+Each geographical position can be represented as a Node or a Geonode. The Node is 
+characterised by planar coordinates, while the Geonode is characterised by 
+geographical coordinates (i.e. latitude and longitude).
+Each node can be marked as active if it represents a position of interest, such as
+a company, a shop, or a monument.
+
+
+< EDGES >
+Each edge connect two different nodes. 
+
+
 """
 
-from typing import Dict, Tuple, List, Union, cast, NewType
-
 import geopy.distance as geodist
+import collections
+import abc
 
 
-# Define a position
-Position = NewType ("Position", Tuple[int, int])
-
-
-# Define a position in geographical coordinates
-Geoposition = NewType ("Geoposition", Tuple[float,float])
+Position = collections.namedtuple ("Position", "x y")
 
 
 
 
 
-
-def _euclidean (pos1 : Position, pos2 : Position) -> int:
+def _euclidean (pos1, pos2) -> int:
 	"""
 	This method returns the euclidean distance between two positions.
 
@@ -31,7 +37,9 @@ def _euclidean (pos1 : Position, pos2 : Position) -> int:
 	:return: The euclidean distance.
 
 	"""
-	return int(((pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2)**0.5)
+	return int(((pos1.x - pos2.x)**2 + (pos1.y - pos2.y)**2)**0.5)
+
+
 
 
 
@@ -46,7 +54,7 @@ class Node (object):
 	An instance of this class represents a node of the Graph.
 
 	"""
-	def __init__ (self, id : int, pos : Tuple[int,int], active : bool = False) -> None:
+	def __init__ (self, id, pos, active=False):
 		"""
 		Initialize.
 		
@@ -57,8 +65,50 @@ class Node (object):
 
 		"""
 		self.id = id
-		self.pos = cast(Position, pos)
+		self.pos = Position(pos[0], pos[1])
 		self.active = active
+
+
+
+	def __eq__ (self, other):
+		"""
+		Magic method that override the equality (==) operator.
+		It returns True if the nodes are in the same position, False otherwise.
+		It raise an Exception if the comparison is between a node and a Geonode.
+
+		:param other: An other node:
+		:return: True if the nodes are in the same position.
+
+		"""
+		if type(other) is Geonode:
+			raise TypeError ("Comparison between a Node and a Geonode")
+
+		if other.pos[0] == self.pos[0] and other.pos[1] == self.pos[1]:
+			return True
+
+		return False
+
+
+
+	def __ne__ (self, other):
+		"""
+		Magic method that override the inequality (!=) operator.
+		It returns True if the nodes are NOT in the same position, False otherwise.
+		It raise an Exception if the comparison is between a node and a Geonode.
+
+		:param other: An other node:
+		:return: True if the nodes are NOT in the same position.
+
+		"""
+		if type(other) is Geonode:
+			raise TypeError ("Comparison between a Node and a Geonode")
+
+		if other.pos[0] != self.pos[0] or other.pos[1] != self.pos[1]:
+			return True
+
+		return False
+
+
 
 
 
@@ -72,7 +122,7 @@ class Geonode (object):
 	if its position is defined by geographical coordinates.
 
 	"""
-	def __init__ (self, id : int, geopos : Tuple[float,float], active : bool = False) -> None:
+	def __init__ (self, id, geopos, active=False):
 		"""
 		Initialize.
 		
@@ -83,8 +133,50 @@ class Geonode (object):
 
 		"""
 		self.id = id
-		self.pos = cast (Geoposition, geopos)
+		self.pos = Position (geopos[0], geopos[1])
 		self.active = active
+
+
+
+
+	def __eq__ (self, other):
+		"""
+		Magic method that override the equality (==) operator.
+		It returns True if the nodes are in the same position, False otherwise.
+		It raise an Exception if the comparison is between a node and a Node.
+
+		:param other: An other node:
+		:return: True if the nodes are in the same position.
+
+		"""
+		if type(other) is Node:
+			raise TypeError ("Comparison between a Node and a Geonode")
+
+		if other.pos[0] == self.pos[0] and other.pos[1] == self.pos[1]:
+			return True
+
+		return False
+
+
+
+	def __ne__ (self, other):
+		"""
+		Magic method that override the inequality (!=) operator.
+		It returns True if the nodes are NOT in the same position, False otherwise.
+		It raise an Exception if the comparison is between a node and a Geonode.
+
+		:param other: An other node:
+		:return: True if the nodes are NOT in the same position.
+
+		"""
+		if type(other) is Node:
+			raise TypeError ("Comparison between a Node and a Geonode")
+
+		if other.pos[0] != self.pos[0] or other.pos[1] != self.pos[1]:
+			return True
+
+		return False
+
 
 
 
@@ -95,21 +187,35 @@ class Geonode (object):
 
 class Edge (object):
 	"""
-
+	An instance of this class represents and edge of the graph connecting two nodes.
 
 	"""
-	def __init__ (self,
-				origin : Union[Node, Geonode],
-				destination : Union[Node, Geonode],
-				length : Optional[int] = None,
-				oneway : bool = False
-				) -> None:
+	def __init__ (self, origin, destination, length=None, oneway=False):
 		"""
 		Initialize.
 
+		:attr origin: The node of origin.
+		:attr destination: The node of destination.
+		:attr length: The length of the edge (if not provide is calculated as a 
+						geographic distance in case of Geonodes and euclidean distance
+						in case of Nodes.
+		:attr oneway: If TRUE the edge in runnable only from the origin to the destination.)
+
 
 
 		"""
-		self.nodes : Union[Tuple[Node, Node],Tuple[Geonode, Geonode]] = (origin, destination)
-		self.length = length or int(geodist.geodesic(origin.pos, destination.pos).m) if type(origin) is Geonode else _euclideant(origin.pos, destination.pos)
+		self.origin = origin
+		self.destination = destination
+		self.length = length or int(geodist.geodesic(origin.pos, destination.pos).m) if type(origin) is Geonode else _euclidean(origin.pos, destination.pos)
 		self.oneway = oneway
+
+
+
+	@property
+	def nodes (self):
+		"""
+		It returns origin and destination in a different format.
+
+		"""
+		return self.origin, self.destination
+	
